b5c3a8f3d79cfefacce823b9d844f7e19a53fe26
68mschmitt <68mschmitt@gmail.com>
2025-08-15 20:51:38 -0400

    Updated copilot configuration and dependencies
    Signed-off-by: 68mschmitt <68mschmitt@gmail.com>

--- neovim/.config/nvim/lua/configs/copilot.lua
+++ neovim/.config/nvim/lua/configs/copilot.lua
@@ -1,17 +1,116 @@
-return {
-    "olimorris/codecompanion.nvim",
-    keys = {
-        { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "AI Toggle [C]hat" },
-        { "<leader>an", "<cmd>CodeCompanionChat<cr>", mode = { "n", "v" }, desc = "AI [N]ew Chat" },
-        { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "AI [A]ction" },
-        { "<leader>ga", "<cmd>CodeCompanionChat Add<CR>", mode = { "v" }, desc = "AI [A]dd to Chat" },
-        -- prompts
-        { "<leader>ae", "<cmd>CodeCompanion /explain<cr>", mode = { "v" }, desc = "AI [E]xplain" },
+return
+{
+    {
+        "yetone/avante.nvim",
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        -- ⚠️ must add this setting! ! !
+        build = vim.fn.has("win32") ~= 0
+        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
+        event = "VeryLazy",
+        version = false, -- Never set this value to "*"! Never!
+        ---@module 'avante'
+        ---@type avante.Config
+        opts = {
+            provider = "copilot",
+            auto_suggestions_provider = nil,
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "echasnovski/mini.pick", -- for file_selector provider mini.pick
+            "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+            "ibhagwan/fzf-lua", -- for file_selector provider fzf
+            "stevearc/dressing.nvim", -- for input provider dressing
+            "folke/snacks.nvim", -- for input provider snacks
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+            {
+                "zbirenbaum/copilot.lua",
+                event = "VeryLazy",
+                config = function()
+                    require('copilot').setup({
+                        suggestion = {
+                            enabled = true,
+                            auto_trigger = true,
+                            accept = false
+                        },
+                        panel = {
+                            enabled = false
+                        },
+                        filetypes = {
+                            markdown = true,
+                            help = true,
+                            html = true,
+                            javascript = true,
+                            typescript = true,
+                            ["*"] = true
+                        }
+                    })
+
+                    vim.keymap.set("i", '<S-Tab>', function()
+                        if require("copilot.suggestion").is_visible() then
+                            require("copilot.suggestion").accept()
+                        else
+                            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", false)
+                        end
+                    end, {
+                    silent = true,
+                })
+            end
+        }, -- for providers='copilot'
+        {
+            -- support for image pasting
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- recommended settings
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true,
+                    },
+                    -- required for Windows users
+                    use_absolute_path = true,
+                },
+            },
+        },
+        {
+            -- Make sure to set this up properly if you have lazy=true
+            'MeanderingProgrammer/render-markdown.nvim',
+            opts = {
+                file_types = { "markdown", "Avante" },
+            },
+            ft = { "markdown", "Avante" },
+        },
     },
-    opts = {},
+},
+{
+    "ravitemer/mcphub.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "github/copilot.vim",
     },
+    build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
+    config = function()
+        require("mcphub").setup()
+    end
 }
+}
+-- return {
+    --     "olimorris/codecompanion.nvim",
+    --     keys = {
+        --         { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "AI Toggle [C]hat" },
+        --         { "<leader>an", "<cmd>CodeCompanionChat<cr>", mode = { "n", "v" }, desc = "AI [N]ew Chat" },
+        --         { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "AI [A]ction" },
+        --         { "<leader>ga", "<cmd>CodeCompanionChat Add<CR>", mode = { "v" }, desc = "AI [A]dd to Chat" },
+        --         -- prompts
+        --         { "<leader>ae", "<cmd>CodeCompanion /explain<cr>", mode = { "v" }, desc = "AI [E]xplain" },
+        --     },
+        --     opts = {},
+        --     dependencies = {
+            --         "nvim-lua/plenary.nvim",
+            --         "nvim-treesitter/nvim-treesitter",
+            --         "github/copilot.vim",
+            --     },
+            -- }
