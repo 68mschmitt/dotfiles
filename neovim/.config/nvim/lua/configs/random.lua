7ca91f776460eeeb013ad34de195f451c4344687
68mschmitt <68mschmitt@gmail.com>
2025-10-08 15:27:47 -0400

    Add a picker for my notes and add keymap for cellular automation

--- neovim/.config/nvim/lua/configs/random.lua
+++ neovim/.config/nvim/lua/configs/random.lua
@@ -11,9 +11,6 @@ local cellular_automaton = {
     'eandrju/cellular-automaton.nvim',
     lazy = false,
     config = function()
-        vim.keymap.set("n", "<leader>mir", function() vim.cmd([[CellularAutomaton make_it_rain]]) end )
-        vim.keymap.set("n", "<leader>sbl", function() vim.cmd([[CellularAutomaton scramble]]) end )
-        vim.keymap.set("n", "<leader>gol", function() vim.cmd([[CellularAutomaton game_of_life]]) end )
         require('configs.cellular-automaton.horizontal-slide')
         require('configs.cellular-automaton.slide-left')
         require('configs.cellular-automaton.fireworks')
@@ -24,6 +21,11 @@ local cellular_automaton = {
         require("cellular-automaton").register_animation(require('configs.cellular-automaton.star-wars'))
         require("cellular-automaton").register_animation(require('configs.cellular-automaton.wisp'))
         require("cellular-automaton").register_animation(require('configs.cellular-automaton.inferno'))
+        vim.keymap.set("n", "<leader>mir", function() vim.cmd([[CellularAutomaton make_it_rain]]) end )
+        vim.keymap.set("n", "<leader>sbl", function() vim.cmd([[CellularAutomaton scramble]]) end )
+        vim.keymap.set("n", "<leader>gol", function() vim.cmd([[CellularAutomaton game_of_life]]) end )
+        vim.keymap.set("n", "<leader>wis", function() vim.cmd([[CellularAutomaton wisp]]) end )
+        vim.keymap.set("n", "<leader>fw", function() vim.cmd([[CellularAutomaton fireworks]]) end )
     end
 };
 
