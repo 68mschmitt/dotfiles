7570e4f5868fce565f3f8d57f0e92339f5674c98
68mschmitt <68mschmitt@gmail.com>
2025-10-10 15:06:38 -0400

    Runner

--- neovim/.config/nvim/lua/configs/random.lua
+++ neovim/.config/nvim/lua/configs/random.lua
@@ -19,6 +19,7 @@ local cellular_automaton = {
         require('configs.cellular-automaton.ripple')
         require('configs.cellular-automaton.blackhole')
         require("configs.cellular-automaton.snowtown").register()
+        require("configs.cellular-automaton.runner").register()
         require("cellular-automaton").register_animation(require('configs.cellular-automaton.updraft'))
         require("cellular-automaton").register_animation(require('configs.cellular-automaton.ember-rise'))
         require("cellular-automaton").register_animation(require('configs.cellular-automaton.glitch_drift'))
