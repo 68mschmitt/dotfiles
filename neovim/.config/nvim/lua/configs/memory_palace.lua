b3a5182d0594fb7d72492eef6493447eef561f51
68mschmitt <68mschmitt@gmail.com>
2025-10-14 13:26:34 -0400

    Add template to note plugin config

--- neovim/.config/nvim/lua/configs/memory_palace.lua
+++ neovim/.config/nvim/lua/configs/memory_palace.lua
@@ -1,3 +1,7 @@
+local note_templates = {
+    profile = "# {{title}}\n{{datetime}}\n---\n\n# Personal\n\n# Professional\n\n---\n\n# Follow Up Questions",
+}
+
 local memory_palace = {
     '68mschmitt/memory-palace.nvim',
     dev = true,
@@ -5,11 +9,13 @@ local memory_palace = {
     cmd = { "NewNote", "SortNote" },
     keys = {
         { "<leader>nn", "<cmd>NewNote<cr>",  desc = "New note (inbox)" },
+        { "<leader>nc", "<cmd>CreateNote<cr>", desc = "Create new note with intended location" },
         { "<leader>ns", "<cmd>SortNote<cr>", desc = "Sort/move note" },
     },
     opts = {
         base_dir  = "~/projects/second-brain/Vault/Vault/MemoryPalaceRPG/Islands",
         inbox_dir = "~/projects/second-brain/Vault/Vault/MemoryPalaceRPG/Islands/00-Tutorial-Island/tutorial-pen",
+        templates = note_templates,
     },
 }
 
