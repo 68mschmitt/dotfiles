c5c448ba680e4b7c22027f50e22941d274b66dd4
68mschmitt <68mschmitt@gmail.com>
2025-10-30 15:31:49 -0400

    thought works plugin

--- neovim/.config/nvim/lua/configs/memory_palace.lua
+++ neovim/.config/nvim/lua/configs/memory_palace.lua
@@ -1,6 +1,8 @@
 local note_templates = {
-    profile = "# {{title}}\n{{datetime}}\n---\n\n# Personal\n\n{{content}}\n\n# Professional\n\n---\n\n# Follow Up Questions",
-    distill = "# {{title}}\n{{datetime}}\n---\n\n{{content}}\n\n# Summarize / Distillation\n---\n- Key takeaways\n- Insights or questions\n- Actions to take\n- Cross-references",
+    profile =
+    "# {{title}}\n{{datetime}}\n---\n\n# Personal\n\n{{content}}\n\n# Professional\n\n---\n\n# Follow Up Questions",
+    distill =
+    "# {{title}}\n{{datetime}}\n---\n\n{{content}}\n\n# Summarize / Distillation\n---\n- Key takeaways\n- Insights or questions\n- Actions to take\n- Cross-references",
 }
 
 local memory_palace = {
@@ -9,9 +11,9 @@ local memory_palace = {
     lazy = false,
     cmd = { "NewNote", "SortNote" },
     keys = {
-        { "<leader>nn", "<cmd>NewNote<cr>",  desc = "New note (inbox)" },
+        { "<leader>nn", "<cmd>NewNote<cr>",    desc = "New note (inbox)" },
         { "<leader>nc", "<cmd>CreateNote<cr>", desc = "Create new note with intended location" },
-        { "<leader>ns", "<cmd>SortNote<cr>", desc = "Sort/move note" },
+        { "<leader>ns", "<cmd>SortNote<cr>",   desc = "Sort/move note" },
     },
     opts = {
         base_dir  = "~/projects/second-brain/Vault/Vault/MemoryPalaceRPG/Islands",
@@ -25,7 +27,22 @@ local dirquest = {
     dev = true,
 }
 
+local thoughtworks = {
+    '68mschmitt/thoughtworks.nvim',
+    dev = true,
+    lazy = false,
+    opts = {
+        root_dir = vim.fn.expand("~/projects/second-brain/thoughtworks"),
+        inbox_dir = "inbox",
+        keymaps = {
+            enabled = true,
+            prefix = "<leader>n",
+        }
+    },
+}
+
 return {
-    memory_palace,
-    dirquest
+    -- memory_palace,
+    dirquest,
+    thoughtworks
 }
