1a23982e12b7b4bb283ca1774a76d3725012aeba
68mschmitt <68mschmitt@gmail.com>
2026-03-31 15:24:13 -0400

    markdown preview

---
+++ neovim/.config/nvim/lua/configs/mdprev.lua
@@ -0,0 +1,7 @@
+-- Browser-based Markdown preview (mermaid, KaTeX, PlantUML, etc.)
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npx --yes yarn install",
+}
