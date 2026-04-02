c4004da154e144ab91e34cdafc614811d3da891f
68mschmitt <68mschmitt@gmail.com>
2026-04-01 21:49:00 -0400

    gloss

---
+++ neovim/.config/nvim/lua/configs/gloss.lua
@@ -0,0 +1,12 @@
+local gloss = {
+  "68mschmitt/gloss.nvim",
+  cmd = {
+    "GlossAdd", "GlossDelete", "GlossExpand", "GlossCollapse",
+    "GlossToggle", "GlossNext", "GlossPrev", "GlossAttach",
+  },
+  opts = {},
+}
+
+return {
+    gloss,
+}
