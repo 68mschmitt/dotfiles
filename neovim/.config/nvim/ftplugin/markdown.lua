f0a79d8c34f774562249e080b0fc4a53b648a861
68mschmitt <68mschmitt@gmail.com>
2026-04-08 09:35:01 -0400

    Update markdown formatting

--- neovim/.config/nvim/ftplugin/markdown.lua
+++ neovim/.config/nvim/ftplugin/markdown.lua
@@ -1,7 +1,8 @@
 -- Prose formatting
-vim.bo.textwidth = 80
+vim.bo.textwidth = 100
 vim.opt_local.formatoptions:append("t") -- auto-wrap text at textwidth
 vim.opt_local.linebreak = true          -- wrap at word boundaries when 'wrap' is on
+vim.opt_local.wrap = true               -- Set wrap on for md files that overflow
 
 -- Insert horizontal rule and drop to new line
 vim.keymap.set("i", "<C-h>", "---<cr><esc>o", {
