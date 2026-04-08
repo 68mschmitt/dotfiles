3bf28745e44029c5ebd7f0e2161341559b946975
68mschmitt <68mschmitt@gmail.com>
2026-04-08 09:35:12 -0400

    gloss plugin

--- neovim/.config/nvim/lua/configs/gloss.lua
+++ neovim/.config/nvim/lua/configs/gloss.lua
@@ -1,8 +1,19 @@
 local gloss = {
   "68mschmitt/gloss.nvim",
   cmd = {
-    "GlossAdd", "GlossDelete", "GlossExpand", "GlossCollapse",
-    "GlossToggle", "GlossNext", "GlossPrev", "GlossAttach",
+    "GlossAdd", "GlossDelete", "GlossEdit", "GlossToggle",
+    "GlossToggleAll", "GlossNext", "GlossPrev", "GlossAttach",
+    "GlossList",
+  },
+  keys = {
+    { "<leader>ga", "<cmd>GlossAdd<cr>", mode = { "n", "v" }, desc = "Add gloss" },
+    { "<leader>gd", "<cmd>GlossDelete<cr>", desc = "Delete gloss" },
+    { "<leader>ge", "<cmd>GlossEdit<cr>", desc = "Edit gloss" },
+    { "<leader>gt", "<cmd>GlossToggle<cr>", desc = "Toggle gloss" },
+    { "<leader>gT", "<cmd>GlossToggleAll<cr>", desc = "Toggle all glosses" },
+    { "<leader>gn", "<cmd>GlossNext<cr>", desc = "Next gloss" },
+    { "<leader>gp", "<cmd>GlossPrev<cr>", desc = "Previous gloss" },
+    { "<leader>gl", "<cmd>GlossList<cr>", desc = "List glosses" },
   },
   opts = {},
 }
