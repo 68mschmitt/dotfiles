25cf50fb04384805c9736e49219b4639d3c39f00
68mschmitt <68mschmitt@gmail.com>
2026-06-01 15:33:21 -0400

    updates to the config obsidian

--- neovim/.config/nvim/lua/configs/pkm.lua
+++ neovim/.config/nvim/lua/configs/pkm.lua
@@ -102,7 +102,24 @@ local markview = {
     end,
 };
 
+local obsidian = {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- use latest release, remove to use latest commit
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false, -- this will be removed in 4.0.0
+    workspaces = {
+      {
+        name = "personal",
+        path  = "~/projects/second-brain",
+      }
+    },
+  },
+}
+
 return {
     katasync,
     markview,
+    obsidian,
 }
