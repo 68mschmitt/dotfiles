4ccde469fea84ba6d5cbf6f8a5ca114229640d0d
68mschmitt <68mschmitt@gmail.com>
2026-07-23 12:11:22 -0400

    octo pr review plugin

--- neovim/.config/nvim/lua/configs/git.lua
+++ neovim/.config/nvim/lua/configs/git.lua
@@ -14,7 +14,33 @@ local fugitive = {
     end
 };
 
+local octo = {
+    'pwntester/octo.nvim',
+    cmd = "Octo",
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'folke/snacks.nvim',
+        'nvim-tree/nvim-web-devicons',
+    },
+    opts = function()
+        -- Highlight PR/issue buffers as markdown
+        vim.treesitter.language.register("markdown", "octo")
+        return {
+            -- "default" = vim.ui.select (styled by snacks via ui_select=true).
+            -- The snacks picker previews octo:// buffers and races its async
+            -- gh load -> "Invalid buffer id". octo exposes no preview toggle.
+            picker = "default",
+            enable_builtin = true,
+        }
+    end,
+    keys = {
+        { "<leader>gr", "<cmd>Octo pr list<cr>", desc = "Octo: review PRs" },
+        { "<leader>gO", "<cmd>Octo<cr>",         desc = "Octo: command palette" },
+    },
+};
+
 return {
     gitsigns,
-    fugitive
+    fugitive,
+    octo
 }
